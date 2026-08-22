3170ed22432878c0f484ecdccf11f167096ef318
emmy grammy <ojeabuo24@gmail.com>
2026-08-22 01:26:14 +0100

    src

---
+++ src/config/db.ts
@@ -0,0 +1,27 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+// console.log('🔥🔥🔥 DB FILE LOADED 🔥🔥🔥');
+
+// console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL);
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+// console.log('Testing database connection...');
+
+export async function testDBConnection() {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        console.log('✅ Connected to database');
+        console.log('Database time:', result.rows[0]);
+    } catch (error) {
+        console.error('❌ Database connection failed');
+        console.error(error);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { pool, testDBConnection } from './config/db.js';
+
 
 dotenv.config();
 
-const app= express();
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.send('User Management system running')
-})
+app.get('/', (req, res) => {
+    res.send('User Management system running');
+});
 
-const PORT= process.env.PORT || 5000;
-app.listen(PORT, ()=>{
+// Test database connection
+testDBConnection();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
